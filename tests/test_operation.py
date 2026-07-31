14bbcb7382b4945e0e236ce07446164773fcbb9c
Ashok Mudgapalli <amudgapalli@gmail.com>
2026-07-30 20:31:42 -0500

    Unit test case updated

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,4 +1,4 @@
-from src.math_operations import add, subtract,
+from src.math_operations import add, subtract
 
 def test_add():
     assert add(2, 3) == 5
@@ -9,5 +9,3 @@ def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(1, 1) == 0
     assert subtract(0, 0) == 0
-
-    
